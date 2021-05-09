0904b97a98d1538851788c2377be770abc727c5b
edouard <m@edouard.paris>
2021-05-09 23:55:19 +0200

    Add bitcoind

---
+++ docker/bitcoind/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+# containers on linux share file permissions with hosts.
+# assigning the same uid/gid from the host user
+# ensures that the files can be read/write from both sides
+if ! id bitcoin > /dev/null 2>&1; then
+  USERID=${USERID:-1000}
+  GROUPID=${GROUPID:-1000}
+
+  echo "adding user bitcoin ($USERID:$GROUPID)"
+  groupadd -f -g $GROUPID bitcoin
+  useradd -r -u $USERID -g $GROUPID bitcoin
+fi
+
+exec bitcoind
