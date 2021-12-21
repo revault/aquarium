9a63f5fc6bda99974cce14f4e04a5f80cdaf01b4
edouard <m@edouard.paris>
2021-12-21 17:28:30 +0100

    update binaries version for 0.3

--- aquarium.py
+++ aquarium.py
@@ -32,11 +32,11 @@ MIRADORD_SRC_DIR = os.path.join(SRC_DIR, "miradord")
 REVAULTD_SRC_DIR = os.path.join(SRC_DIR, "revaultd")
 REVAULT_GUI_SRC_DIR = os.path.join(SRC_DIR, "revault-gui")
 SHELL = os.getenv("SHELL", "bash")
-COORDINATORD_VERSION = os.getenv("COORDINATORD_VERSION", "master")
-COSIGNERD_VERSION = os.getenv("COSIGNERD_VERSION", "master")
-MIRADORD_VERSION = os.getenv("MIRADORD_VERSION", "master")
-REVAULTD_VERSION = os.getenv("REVAULTD_VERSION", "master")
-REVAULT_GUI_VERSION = os.getenv("REVAULT_GUI_VERSION", "master")
+COORDINATORD_VERSION = os.getenv("COORDINATORD_VERSION", "0.3")
+COSIGNERD_VERSION = os.getenv("COSIGNERD_VERSION", "0.3")
+MIRADORD_VERSION = os.getenv("MIRADORD_VERSION", "0.0.1")
+REVAULTD_VERSION = os.getenv("REVAULTD_VERSION", "0.3")
+REVAULT_GUI_VERSION = os.getenv("REVAULT_GUI_VERSION", "0.3")
 WITH_GUI = os.getenv("WITH_GUI", "1") == "1"
 WITH_ALL_HWS = os.getenv("WITH_ALL_HWS", "0") == "1"
 
@@ -69,7 +69,7 @@ def build_src(src_dir, version, git_url):
         subprocess.check_call(["git", "-C", f"{SRC_DIR}", "clone", git_url])
 
     subprocess.check_call(["git", "-C", f"{src_dir}", "fetch", "origin", version])
-    subprocess.check_call(["git", "-C", f"{src_dir}", "checkout", f"origin/{version}"])
+    subprocess.check_call(["git", "-C", f"{src_dir}", "checkout", f"tags/{version}"])
     subprocess.check_call(
         ["cargo", "build", "--manifest-path", f"{src_dir}/Cargo.toml"]
     )
